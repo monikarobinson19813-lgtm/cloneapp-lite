d573ad1b2a1bdacdfa7eab0fa8a323ca7983008c
monikarobinson19813-lgtm <monikarobinson19813@gmail.com>
2026-09-24 21:00:53 +0530

    refactor: remove Xposed flag from app model

--- app/src/main/java/top/niunaijun/blackboxa/bean/AppInfo.kt
+++ app/src/main/java/top/niunaijun/blackboxa/bean/AppInfo.kt
@@ -3,4 +3,4 @@ package top.niunaijun.blackboxa.bean
 import android.graphics.drawable.Drawable
 
 
-data class AppInfo(val name:String,val icon:Drawable?,val packageName:String,val sourceDir:String,val isXpModule:Boolean)
+data class AppInfo(val name:String,val icon:Drawable?,val packageName:String,val sourceDir:String)
